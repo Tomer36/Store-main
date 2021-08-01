dbbdd6930dc74b6d4da8f587ecc9b29d17354601
Tomer36 <67845283+Tomer36@users.noreply.github.com>
2021-08-01 14:40:46 +0300

    Revert "Initial commit"
    This reverts commit 1809cd56dc670032422fba9cf65494719ace8729.

--- dbConnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servername ="localhost";
-    $username = "root";
-    $password ="";
-    $db = "mydb-main";
-    $conn = mysqli_connect($servername , $username ,$password ,$db);
-?>
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-session_start();
-session_destroy();//to get out the client from the website
-header("Location:Sign_in.php");//back to backward page
- ?>
