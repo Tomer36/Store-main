ec6df4afe9c52d78e479690e1796f724f2c67e87
Tomer36 <67845283+Tomer36@users.noreply.github.com>
2021-08-02 21:29:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
     <script src="store.js" async></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- jQuery UI -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+
 </head>
 
 <body onload="viewPrevPurchaces3();">
@@ -59,7 +66,7 @@
         <div class="float-child">
             <div class="list">
                 <h3>Type Items To Add:</h3>
-                <input type="text" class="addItemInput" placeholder="Typing.."> &nbsp
+                <input type="text" class="addItemInput" id="search" placeholder="Typing.."> &nbsp
                 <button class="btn btn-primary list-item-button"> Add Item </button> &nbsp&nbsp
                 <button class="btn btn-danger" type="button" onclick="MakeNewList()" style="background-color: red;color: white;">Make a new List</button> <br> <br>
                 <ul>
@@ -144,6 +151,14 @@
         x.innerHTML = "";
 
     }
+</script>
+
+<script type="text/javascript">
+    $(function() {
+        $("#search").autocomplete({
+            source: 'autocomplete.php',
+        });
+    });
 </script>
 
 </html>
