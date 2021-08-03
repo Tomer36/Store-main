a0c329a0c4d27316ea1a14bd9ae7f11485deb001
Tomer36 <67845283+Tomer36@users.noreply.github.com>
2021-08-03 23:22:31 +0300

    Update save.php

--- save.php
+++ save.php
@@ -2,7 +2,7 @@
 	include 'dbConnection.php';
 	$title=$_POST['title'];
 	$qty=$_POST['qty'];
-	$sql = "INSERT INTO `final_list`( `title`, `qty` ) 
+	$sql = "INSERT INTO `items`( `title`, `qty` ) 
 	VALUES ('$title','$qty')";
 	if (mysqli_query($conn, $sql)) {
 		echo json_encode(array("statusCode"=>200));
